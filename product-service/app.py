f3e062e24aef9e486a7aa439ced2a19ea109bd4e
Vishal18 <vishalthakur8837@gmail.com>
2026-04-08 16:29:08 +0530

    Testing Pro v3

--- product-service/app.py
+++ product-service/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Product Service is running by Vishal v3!"
+    return " Testing Product Service is running by Vishal v3!"
 
 @app.route('/products')
 def products():
